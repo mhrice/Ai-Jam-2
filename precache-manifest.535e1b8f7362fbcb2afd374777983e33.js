ee096f040f9a5df4a2d86d99f236944c605d6c35
mhrice <matthewrice11@yahoo.com>
2019-06-11 23:07:06 -0700

    Updates

---
+++ precache-manifest.535e1b8f7362fbcb2afd374777983e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0475cc8836c3a921672593df40eed458",
+    "url": "/Ai-Jam-2/index.html"
+  },
+  {
+    "revision": "fe126fd21d95dcffa970",
+    "url": "/Ai-Jam-2/static/css/main.f92d6f57.chunk.css"
+  },
+  {
+    "revision": "26ba4282df6b9a43e6a5",
+    "url": "/Ai-Jam-2/static/js/2.26b148ec.chunk.js"
+  },
+  {
+    "revision": "fe126fd21d95dcffa970",
+    "url": "/Ai-Jam-2/static/js/main.5ba46275.chunk.js"
+  },
+  {
+    "revision": "a309d462c236eaa038bf",
+    "url": "/Ai-Jam-2/static/js/runtime~main.9620b52a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ai-jam-2/precache-manifest.611c06fafd9128cdcf7227d9e7e44981.js"
+  "/Ai-Jam-2/precache-manifest.535e1b8f7362fbcb2afd374777983e33.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ai-jam-2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ai-Jam-2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
